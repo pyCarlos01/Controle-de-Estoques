7107ac7d79222a21a529737989bddfcdb345bd88
Carlos Damião <136815475+pyCarlos01@users.noreply.github.com>
2023-07-25 10:42:09 -0300

    first

--- Controle/settings.py
+++ Controle/settings.py
@@ -33,7 +33,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["controle-de-estoques-production.up.railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
