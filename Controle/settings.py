9b0455c1d5bd701c21dd2773beb35ce740356830
Carlos Damião <136815475+pyCarlos01@users.noreply.github.com>
2023-07-27 10:28:19 -0300

    settings

--- Controle/settings.py
+++ Controle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 
@@ -83,14 +84,12 @@ WSGI_APPLICATION = 'Controle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-import dj_database_url
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
